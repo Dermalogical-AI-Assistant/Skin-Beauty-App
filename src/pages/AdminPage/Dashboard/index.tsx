090377ef2c7d171760cd75485093436d99f90df6
xenwithu <xenwithu@outlook.com>
2025-04-16 18:55:36 +0700

    set authorization toke

--- src/pages/AdminPage/Dashboard/index.tsx
+++ src/pages/AdminPage/Dashboard/index.tsx
@@ -1,23 +1,30 @@
 import useAuthStore from "../../../stores/AuthStore.ts";
-import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import useTestSetAccessToken from "../../../hooks/useTestSetAccessToken.ts";
 
 const Dashboard: React.FC = () => {
-    const { user, logout } = useAuthStore();
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        logout(); // Gọi hàm logout từ Zustand
-        // navigate("/login"); // Chuyển hướng về trang login
-        window.location.reload();
-    };
+    const { user } = useAuthStore();
 
-    useEffect(() => {
-    console.log("hihi",user);
-    }, [user]);
+    const {fetchUserInfo} = useTestSetAccessToken();
+
+    async function fetchUserInfoData() {
+      const data = await fetchUserInfo.refetch();
+      if (data.isSuccess) {
+        console.log("This is user data: ",data.data.data);
+      }
+    }
 
     return (
         <>
             hello from dashboard
+            <button
+                onClick={() => {
+                  fetchUserInfoData();
+                }}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Fetch User Info
+            </button>
+
             <div>
                 <h1>{user?.username}</h1>
                 <p>{user?.email}</p>
