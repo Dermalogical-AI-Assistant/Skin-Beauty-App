338a2d07bd3c7039bf6eb975983c278a73a68331
Le Van Dat <Lvd.levandat@gmail.com>
2025-06-03 15:34:41 +0700

    fix: admin account menu is not absolute (#32)

--- src/layouts/Admin/AdminLayout.tsx
+++ src/layouts/Admin/AdminLayout.tsx
@@ -19,20 +19,22 @@ const AdminLayout: React.FC<LayoutProps> = ({ children }) => {
     };
 
     return (
-      <div className="flex h-screen font-baloo text-primary-dark bg-primary">
+      <div className="relative flex h-screen font-baloo text-primary-dark bg-primary">
         {/* Sidebar */}
         <div className={"p-5"}>
           <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
         </div>
         {/* Main Content */}
-        <div className={"h-full p-5 flex flex-1 overflow-hidden"}>
+        <div className={"relative h-full p-5 flex flex-1 overflow-hidden"}>
           <div className="h-full flex flex-col flex-1 bg-white/60 rounded-2xl shadow-lg ">
-            <Navbar/>
+            <div className={`relative z-30`}>
+              <Navbar/>
+            </div>
             <main className="relative h-full overflow-hidden">
               <div className="h-full">
                 {/*<div className="sticky top-0 h-6 !bg-gradient-to-b !from-white border !to-white/0 z-10 pointer-events-none"></div>*/}
                 {children || <Outlet />}
-                <div className="sticky bottom-0 h-6 bg-gradient-to-t from-white to-transparent z-10 pointer-events-none rounded-bl-2xl rounded-br-2xl"></div>
+                <div className="sticky  z-10  bottom-0 h-6 bg-gradient-to-t from-white to-transparent pointer-events-none rounded-bl-2xl rounded-br-2xl"></div>
               </div>
             </main>
           </div>
